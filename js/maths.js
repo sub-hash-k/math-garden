5c03e02647b94e73bde27fb8dd7aa7065493f24a
sub-hash-k <68750013+sub-hash-k@users.noreply.github.com>
2024-10-29 19:50:48 +0000

    url file path update

--- js/maths.js
+++ js/maths.js
@@ -17,13 +17,12 @@ function nextQuestion() {
 function checkAnswer() {
 
     const prediction = predictImage();
-    //console.log(`Answer: ${answer}, prediction: ${prediction}`);
 
     if (prediction == answer) {
         score++;
         //console.log(`Correct! Score: ${score}`);
         if (score <= 6) {
-            backgroundImages.push(`url('images/background${score}.svg')`);
+            backgroundImages.push(`url('assets/images/background${score}.svg')`);
             document.body.style.backgroundImage = backgroundImages;
         } else {
             alert('Well done! Your maths garden is in full bloom! Want to start again?');
@@ -33,7 +32,6 @@ function checkAnswer() {
         }
     } else {
         if (score != 0) { score--; }
-        //console.log(`Wrong. Score ${score}`);
 
         alert('Oops! Check your calculations and try writing the number neater next time!');
         setTimeout(function () {
